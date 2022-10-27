0268ff851995c2814dab45c507d887b159a0e285
Bartek0074 <bartekdo123@gmail.com>
2022-10-27 23:05:13 +0200

    added initSnake function

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function getDirection() {
 
 const createBoard = () => {
     console.log(board);
-    for (let i = 1; i < 20; i++) {
-        for (let j = 1; j < 20; j++) {
+    for (let i = 1; i <= 20; i++) {
+        for (let j = 1; j <= 20; j++) {
             const div = document.createElement('div');
             div.className = "board__element";
             div.dataset.y = i;
@@ -23,4 +23,23 @@ const createBoard = () => {
     }
 }
 
+const initSnake = () => {
+    let xPosition = Math.floor(Math.random() * 12) + 5;
+    let yPosition = Math.floor(Math.random() * 12) + 5;
+    console.log(yPosition, xPosition, snakeDirection)
+
+    for (let i = 0; i < 3; i++){
+        if (snakeDirection === 'up') yPosition++;
+        if (snakeDirection === 'down') yPosition--;
+        if (snakeDirection === 'left') xPosition--;
+        if (snakeDirection === 'right') xPosition++;
+
+        const snakeElement = document.querySelector(`[data-x="${xPosition}"][data-y="${yPosition}"]`);
+        console.log(snakeElement);
+        snakeElement.classList.add('snake')
+        snakeElements.unshift(snakeElement);
+    }
+}
+
 createBoard();
+initSnake();
